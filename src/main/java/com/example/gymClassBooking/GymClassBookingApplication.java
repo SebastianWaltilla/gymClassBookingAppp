5c5da599ef073fcdfb6fc6c71675f4e2d522ffd7
sebastian waltilla <sebastianwaltilla@hotmail.com>
2022-01-08 21:10:01 +0100

    initial commit

---
+++ src/main/java/com/example/gymClassBooking/GymClassBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymClassBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymClassBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymClassBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gymClassBooking/GymClassBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymClassBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymClassBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
